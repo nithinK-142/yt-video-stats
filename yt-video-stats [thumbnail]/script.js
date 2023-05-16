6033b93892fd5362d79668e1efeb127cb43527c4
nithin <nithin.sagar.359@gmail.com>
2023-05-16 18:45:48 +0530

    project update (plays video now).

--- yt-video-stats [thumbnail]/script.js
+++ yt-video-stats [thumbnail]/script.js
@@ -13,11 +13,13 @@ $(document).ready(function() {
     var videoId = null;
     var matches = videoUrl.match(/^https?:\/\/(?:www\.)?(?:youtube\.com\/watch\?v=|youtu\.be\/|youtube\.com\/shorts\/)([a-zA-Z0-9_-]+)((?:&(\S*))?)?(?:\?(t=\d+))?/);
 
-    if (matches) {
+    if(matches){
+      // console.log("URL : " +matches[0]);
+      // console.log("Optional : " +matches[2]);
       videoId = matches[1];
     }
 
-    var apiKey = "YOUR_API_KEY"; 
+    var apiKey = "YOUR_API_KEY";
     var apiUrl = "https://www.googleapis.com/youtube/v3/videos?part=statistics,snippet&id=" + videoId + "&key=" + apiKey;
     var dislikesUrl = "https://returnyoutubedislikeapi.com/votes?videoId=" + videoId;
 
@@ -34,7 +36,9 @@ $(document).ready(function() {
         
         var channelTitle = snippet.channelTitle;
         var videoTitle = snippet.title;
-        var videoEmbedUrl = "https://www.youtube.com/embed/" + videoId;
+        var thumbnailUrl = snippet.thumbnails.medium.url;
+        var thumbnailWidth = snippet.thumbnails.medium.width*1.5; 
+        var thumbnailHeight = snippet.thumbnails.medium.height*1.5;
        
         var publishedDate = new Date(snippet.publishedAt).toLocaleDateString();
 
@@ -42,29 +46,29 @@ $(document).ready(function() {
         var likeCount = parseInt(stats.likeCount).toLocaleString("en-IN");
         var commentCount = parseInt(stats.commentCount).toLocaleString("en-IN");
         
-        var video = "<iframe width='560' height='315' src='" + videoEmbedUrl + "' frameborder='0' allowfullscreen></iframe>";
-        $(".video-container").html(video);
+        var image = "<img src='" + thumbnailUrl + "' width='" + thumbnailWidth + "' height='" + thumbnailHeight + "'/>"; // modified code
+        $(".image-container").html(image);
 
-        var output = "<div><b>Title: </b><h3>" + videoTitle + "</h3></div>";
-        output += "<div><b>Channel: </b><h3>" + channelTitle + "</h3></div>";
-        output += "<div><b>Published Date: </b><h3>" + publishedDate + "</h3></div>";
-        output += "<div><b>Views: </b><h3>" + viewCount + "</h3></div>";
-        output += "<div><b>Comments: </b><h3>" + commentCount + "</h3></div>";
-        output += "<div><b>Likes: </b><h3>" + likeCount + "</h3></div>";
+        output = "<div><b>Title: </b><h3>" + videoTitle +"</h3></div>";
+        output += "<div><b>Channel: </b><h3>" + channelTitle +"</h3></div>";
+        output += "<div><b>Published Date: </b><h3>" + publishedDate +"</h3></div>";
+        output += "<div><b>Views: </b><h3>" + viewCount +"</h3></div>";
+        output += "<div><b>Comments: </b><h3>" + commentCount +"</h3></div>";
+        output += "<div><b>Likes: </b><h3>" + likeCount +"</h3></div>";
         
         getDislikes(videoId).then(function(dislikes) {
-          output += "<div><b>Dislikes: </b><h3>" + dislikes.toLocaleString("en-IN") + "</h3><div>";
+          output += "<div><b>Dislikes </b><h3>" + dislikes.toLocaleString("en-IN") +"</h3><div>";
           $(".stats-container").html(output);
-        });
+        })
       });
-    } else {
-      alert("Please enter a valid YouTube video URL!");
-    }
+    } else
+      alert("Please enter a valid YouTube video URL!!!");
   });
   
   $(".fa-times").click(function() {
     $("#video-url").val("");
-    $(".video-container").html("");
+    $(".image-container").html("");
     $(".stats-container").html("");
   });
+  
 });
